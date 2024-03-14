040134ec070659f1bd1b10645683458c0b7494f0
Gabriele Lopez <gabriele.ope@gmail.com>
2024-03-14 09:54:29 +0100

    added colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,15 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      
     },
+    colors: {
+      'text': '#efd9ff',
+      'background': '#0a0014',
+      'primary': '#c773fd',
+      'secondary': '#ac0291',
+      'accent': '#fc14bb',
+     },
   },
   plugins: [],
 } satisfies Config;
