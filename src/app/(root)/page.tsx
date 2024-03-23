235f3aa27247db21615555f26ca290d787eab268
Gabriele Lopez <gabriele.ope@gmail.com>
2024-03-23 18:17:26 +0100

    main home page for driver accounts

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,58 @@
-export default function HomePage() {
-  return (
-    <main className="">
-      
-    </main>
-  );
+import DriverProfile, { DriverProfileProps } from "~/components/Profile/DriverProfile";
+import { validateRequest } from "~/server/auth";
+import { db } from "~/server/db";
+
+export default async function HomePage() {
+
+  const session = await validateRequest();
+
+  if(session.user?.role == "DRIVER") {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.user.id
+      },
+      select: {
+        username: true,
+        name: true,
+        surname: true,
+        driverInfo: {
+          select: {
+            profilePic: true,
+            vehicles: {
+              select: {
+                model: true,
+                licensePlate: true
+              }
+            }
+          }
+        }
+      }
+    })
+    const ratings = await db.rating.aggregate({
+      where: {
+        userId: session.user.id
+      },
+      _avg: {
+        star: true
+      }
+    })
+    if(user && ratings) {
+      const props: DriverProfileProps = {
+        name: user.name,
+        surname: user.surname,
+        username: user.username,
+        pfp: user.driverInfo!.profilePic.toString('base64'),
+        vehicles: user.driverInfo!.vehicles,
+        rating: ratings._avg.star ?? -1
+      };
+      return(
+        <main className="flex flex-col items-center mt-5">
+          <h1 className="mb-5 text-6xl font-bold text-text/0 text-transparent bg-gradient-to-r from-primary to-accent bg-clip-text">Your Driver Profile</h1>
+          <DriverProfile {...props}></DriverProfile>
+          <h1 className="mb-5 mt-5 text-6xl font-bold text-text/0 text-transparent bg-gradient-to-r from-primary to-accent bg-clip-text">Your Trips</h1>
+          <h1 className="mb-5 mt-5 text-6xl font-bold text-text/0 text-transparent bg-gradient-to-r from-primary to-accent bg-clip-text">Users To Accept</h1>
+        </main>
+      )
+    }
+  }
 }
---
+++ src/components/Profile/DriverProfile.tsx
@@ -0,0 +1,46 @@
+import { Rating } from "@mui/material"
+import Image from "next/image"
+
+export interface DriverProfileProps {
+    username: string,
+    name: string,
+    surname: string,
+    rating: number,
+    pfp: string, //base64
+    vehicles: Vehicle[]
+}
+
+interface Vehicle {
+    licensePlate: string,
+    model: string
+}
+
+export default function DriverProfile(props: DriverProfileProps) {
+    return (
+        <div className="rounded-md p-4 border flex-col flex items-center text-center">
+            <div className="rounded-full border-4 z-10 w-fit bg-white overflow-hidden bg-text/50">
+                <Image width={256} height={256} alt="Profile Picture" src={`data:image/png;base64,${props.pfp}`}></Image>
+            </div>
+            <span className="text-3xl">{props.name} {props.surname}</span>
+            <span className="text-lg text-text/80 mb-2"> {props.username} </span>
+            <Rating className="mb-2" readOnly value={props.rating}></Rating>
+            {props.rating == -1 ? <span className="text-text/70 text-sm mb-2">This user does not have any ratings</span> : null}
+            <div className="text-left w-full">
+            {(() => {
+                if(props.vehicles.length == 0) {
+                    return <span>This user has not added any vehicles</span>
+                }
+                return (
+                    <>
+                    <span className="text-lg">Vehicles:</span>
+                    {props.vehicles.map((item) => {
+                        return <span key={item.licensePlate} className="block font-bold text-text/0 text-transparent bg-gradient-to-r from-primary to-accent bg-clip-text">{item.model}</span>
+                    })}
+                    </>
+                )
+            })()}
+            </div>
+        </div>
+    )
+}
+//TODO add all trips that the driver has posted
